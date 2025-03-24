5a4f8c06d399cce0870727323453174d8357cd95
Wscxw Gswef <d3midovk@gmail.com>
2025-03-25 01:26:45 +0300

    Добавьте файлы проекта.

---
+++ EmailRegex/EmailRegex.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+#include <regex>
+#include <string>
+
+using namespace std;
+
+bool CheckEnail(const string& email) 
+{
+    const regex pattern( R"(^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$)");
+
+    return regex_match(email, pattern);
+}
+
+int main(int argc, char* argv[]) {
+    string email;
+
+    if (argc > 1) 
+    {
+        email = argv[1];
+    }
+    else 
+    {
+        cout << "Enter email address: ";
+        getline(cin, email);
+    }
+
+    if (CheckEnail(email))
+    {
+        cout << "Valid email address" << endl;
+    }
+    else
+    {
+        cout << "Invalid email address" << endl;
+    }
+
+    return 0;
+}
+
